ee1ac36bd24bb58090e74dc3f64133722aafb934
Talha arshad <arshadtalha591@gmail.com>
2022-09-28 13:54:32 +0500

    Routing completed

---
+++ src/components/create-exercise.component.js
@@ -0,0 +1,11 @@
+import React, { Component } from "react";
+
+export default class CreateExercises extends Component {
+    render() {
+        return (
+            <div>
+                <p>You Are on the Create Exercise Component!</p>
+            </div>
+        )
+    }
+}
